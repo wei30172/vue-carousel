a14f0b33b2bb392686dfcbd478e4fa9c078dc9fc
claireWei <claire610323@gmail.com>
2022-08-20 22:13:13 +0800

    add slide transition

--- src/components/Slide.vue
+++ src/components/Slide.vue
@@ -12,4 +12,17 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+}
+.slide-enter-to,
+.slide-leave-from {
+  opacity: 1;
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.6s ease-in-out;
+}
+</style>
